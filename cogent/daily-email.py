cf965a25ba87a5bfdf3aadc81ee52ee2f15a693c
james brusey <james.brusey@gmail.com>
2011-12-26 07:46:50 +0000

    header and footer methods so we can tell if anything was reported

--- cogent/daily-email.py
+++ cogent/daily-email.py
@@ -11,66 +11,72 @@ import platform
 import smtplib
 import re
 
+
 host=platform.node()
 me = "yield@"+host
 _DBURL = "mysql://chuser@localhost/ch?connect_timeout=1"
 you="chuser@localhost"
 
 
-
-def run_reports(dry_run=False, time_queries=False):
-
-   html = ["""From: """+host+"""<"""+me+""">
+def header():
+    return """From: """+host+"""<"""+me+""">
 To: """+you+"""
 MIME-Version: 1.0
 Content-type: text/html
 Subject: """+host+""" yield
-"""]
+<html><head></head><body>"""
 
-   html.append("<html><head></head><body>")
+def footer():
+    return "</body></html>"
 
-   try:
-      session = Session()
+def run_reports(dry_run=False, time_queries=False):
 
-      if time_queries:
-          start_time = time.time()
+    try:
+        session = Session()
 
-      for (report, method) in reports:
-          html.extend(method(session))
-          if time_queries:
-              html.append("<p>%s took: %ld secs</p>" % (report, time.time() - start_time))
-              start_time = time.time()
+        if time_queries:
+            start_time = time.time()
 
+        html = []
+        for (report, method) in reports:
+            html.extend(method(session))
+            if time_queries:
+                html.append("<p>%s took: %ld secs</p>" % (report, time.time() - start_time))
+                start_time = time.time()
 
-      html.append("</body></html>")
-         
-   finally:
-      session.close()    
-  
-   # Send the message via local SMTP server.
-   if dry_run:
-      print "".join(html)
-   else:
-      s = smtplib.SMTP('localhost')
-      s.sendmail(me,you,"".join(html))
+        if len(html) == 0:
+            html = ['No status updates to report']
+
+    finally:
+        session.close()    
+
+    message = header() + "".join(html) + footer()
+
+    if dry_run:
+        print message
+    else:
+        # Send the message via local SMTP server.
+
+        s = smtplib.SMTP('localhost')
+        s.sendmail(me, you, message)
    
 if __name__ == "__main__":
-   from optparse import OptionParser
+    from optparse import OptionParser
 
-   parser = OptionParser()
-   parser.add_option("-n", "--dry-run", 
-                     action="store_true",
-                     default=False,
-                     help="don't send e-mail--just print to stdout")
-   parser.add_option("-t", "--time-queries", 
-                     action="store_true",
-                     default=False,
-                     help="time how long each query takes")
+    parser = OptionParser()
+    parser.add_option("-n", "--dry-run", 
+                      action="store_true",
+                      default=False,
+                      help="don't send e-mail--just print to stdout")
+    parser.add_option("-t", "--time-queries", 
+                      action="store_true",
+                      default=False,
+                      help="time how long each query takes")
 
-   (options, args) = parser.parse_args()
-   
-   engine = create_engine(_DBURL, echo=False)
-   Base.metadata.create_all(engine)
-   init_model(engine)
+    (options, args) = parser.parse_args()
+    
+    engine = create_engine(_DBURL, echo=False)
+    Base.metadata.create_all(engine)
+    init_model(engine)
 
-   run_reports(dry_run=options.dry_run, time_queries=options.time_queries)
+    run_reports(dry_run=options.dry_run, time_queries=options.time_queries)
