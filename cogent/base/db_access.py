2e4b6a1735a60f394faa871d869bb2a4d975d5d5
ross wilkins87 <ross.wilkins87@googlemail.com>
2012-02-22 16:12:06 +0000

    added all types to reading types

--- cogent/base/db_access.py
+++ cogent/base/db_access.py
@@ -9,8 +9,7 @@ import scipy.stats as stats
 from cogent.base.model import *
 
 
-
-reading_types = ['temperature', '', 'humidity', '', 'tsr', 'par', 'battery', '', 'co2', 'aq', 'voc', 'cc']
+reading_types = ['temperature', 'd_temperature', 'humidity', 'd_humidity', 'tsr', 'par', 'battery', 'd_battery', 'co2', 'aq', 'voc', 'cc','duty','error','size_v1','cc_min','cc_max','cc_kwh']
 node_types = ['standard', 'electricity', 'co2', 'aq', 'heat_meter']
 reading_limits = {
         'temperature' : (0.0, 50.0),
@@ -147,7 +146,7 @@ def get_node_locations_by_house(session, house_id, include_external=True):
     return node_details
 
 
-def get_data_by_type(session, reading_type, start_time = datetime.fromtimestamp(0), end_time = datetime.now(), postprocess=True):
+def get_data_by_type(session, reading_type, start_time = datetime.fromtimestamp(0), end_time = datetime.now(), postprocess=True, cal_func=get_calibration):
     rows = _query_by_type(session, reading_type, start_time, end_time, filter_values = False)
     rows.order_by(Reading.time)
 
@@ -160,14 +159,14 @@ def get_data_by_type(session, reading_type, start_time = datetime.fromtimestamp(
     
     if postprocess:
         for node_id,values in data.iteritems():
-            m, c = get_calibration(session, node_id, reading_type)
+            m, c = cal_func(session, node_id, reading_type)
             data[node_id] = [(row[0], m * row[1] + c) for row in values]
         data = clean_data(data, reading_type)
     
     return data
 
 
-def get_data_by_node_and_type(session, node_id, reading_type, start_time = datetime.fromtimestamp(0), end_time = datetime.now(), postprocess=True):
+def get_data_by_node_and_type(session, node_id, reading_type, start_time = datetime.fromtimestamp(0), end_time = datetime.now(), postprocess=True, cal_func=get_calibration):
     rows = _query_by_node_and_type(session, node_id, reading_type, start_time, end_time, filter_values = False)
     rows.order_by(Reading.time)
     
@@ -176,7 +175,7 @@ def get_data_by_node_and_type(session, node_id, reading_type, start_time = datet
         data.append((time, value))
 
     if postprocess:
-        m, c = get_calibration(session, node_id, reading_type)
+        m, c = cal_func(session, node_id, reading_type)
         data = [(row[0], m * row[1] + c) for row in data]
         data = clean_data(data, reading_type)
     
