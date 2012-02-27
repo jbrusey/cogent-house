b5f1a0f046cf31a4bcdb059959a84505dc6806de
Dan Goldsmith <djgoldsmith@googlemail.com>
2012-02-27 14:12:20 +0000

    Meta Functions Synhronised

--- cogent/base/model/weather.py
+++ cogent/base/model/weather.py
@@ -1,10 +1,48 @@
-from sqlalchemy.ext.declarative import declarative_base
+"""
+Table to hold details of Weathers
+
+.. module:: weather
+
+.. codeauthor::  Ross Wiklins 
+.. codeauthor::  James Brusey
+.. codeauthor::  Daniel Goldsmith <djgoldsmith@googlemail.com>
+
+"""
+
+
+import sqlalchemy
+import logging
+log = logging.getLogger(__name__)
+
+import meta
+Base = meta.Base
+
+
 from sqlalchemy import Table, Column, Integer, String, MetaData, ForeignKey, DateTime, Float, Boolean
 from sqlalchemy.orm import relationship, backref
 
-from cogent.base.model.meta import Base
 
 class Weather(Base):
+    """
+    Table to deal with Weather
+
+    :var DateTime time: 
+    :var Float outTemp:
+    :var Float outHum:
+    :var Float dew:
+    :var Float gust:
+    :var Float wSpeed:
+    :var Float wDir:
+    :var Float wChill:
+    
+    :var Float apparentTemp:
+    :var Float rain:
+    :var Float pressure:
+    :var Float tempIn:
+    :var Float humIn:
+    
+    """
+
     __tablename__ = "Weather"
 
     time = Column(DateTime,primary_key=True)
