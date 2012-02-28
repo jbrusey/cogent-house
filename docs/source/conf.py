009204af33c9fc9f8a84d9f8c8237bf796936dc0
Dan Goldsmith <djgoldsmith@googlemail.com>
2012-02-28 15:33:28 +0000

    Sphinx Documentation Configuration and models updated

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -17,7 +17,9 @@ import sys, os
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
-#sys.path.insert(0,os.path.abspath('../../'))
+sys.path.insert(0,os.path.abspath('../../'))
+#print "="*40
+#print sys.path
 #sys.path.insert(0,"/home/dang/Progamming/Pyramid/cogent-viewer/")
 # -- General configuration -----------------------------------------------------
 
