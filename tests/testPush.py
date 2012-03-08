5c3c5a0b2294474865a4ab509d4a84e1424fc821
Dan Goldsmith <djgoldsmith@googlemail.com>
2012-03-08 09:51:16 +0000

    Pushing Tests for connection written

--- tests/testPush.py
+++ tests/testPush.py
@@ -45,20 +45,19 @@ class TestPush(testmeta.BaseTestCase):
 
         #This is a little bit hackey, but it works quite nicely at the moment.
         #Basically creates an empty remote database that we can connect to.
-        # print "Initialising Remote Engine"
+
+        #As well as the Push we want to create a direct connection to the remote database
+        #This means we can query to ensure the output from each is the same
         engine = create_engine(REMOTE_URL)
-        models.initialise_sql(engine,False)
+        models.initialise_sql(engine)
         remoteSession = sqlalchemy.orm.sessionmaker(bind=engine)
         self.remoteSession = remoteSession
         self.engine = engine
 
-        remoteEngine = sqlalchemy.create_engine("sqlite:///remote.db")
-        localEngine =  sqlalchemy.create_engine("sqlite:///test.db")
+        self.remoteEngine = sqlalchemy.create_engine(REMOTE_URL)
+        self.localEngine =  sqlalchemy.create_engine(LOCAL_URL)
 
-        push = Pusher.Pusher()
-        push.init_remote(remoteEngine)
-        push.init_local(localEngine)
-        self.push = push
+
          #push.testRemoteQuery()
          #push.testLocalQuery()
         
@@ -66,27 +65,54 @@ class TestPush(testmeta.BaseTestCase):
         #What is pretty cool is the idea we can also  recreate the database to clean it out each time
         #models.initialise_sql(self.engine,True)
 
+    # def testSetup(self):
+    #     print "Testing setup or remote and local database"
+    #     #Query from local database
+    #     localSession = testmeta.Session()
+    #     remoteSession = self.remoteSession()
 
-    def testSetup(self):
-        print "Testing setup or remote and local database"
-        #Query from local database
-        localSession = testmeta.Session()
-        remoteSession = self.remoteSession()
+    #     print "Querying Local"
+    #     theQry = localSession.query(models.RoomType)
+    #     for item in theQry:
+    #         print "--> {0}".format(item)
 
-        print "Querying Local"
-        theQry = localSession.query(models.RoomType)
-        for item in theQry:
-            print "--> {0}".format(item)
-
-        print "Query Remote"
-        theQry = remoteSession.query(models.RoomType)
-        for item in theQry:
-            print "--> {0}".format(item)
+    #     print "Query Remote"
+    #     theQry = remoteSession.query(models.RoomType)
+    #     for item in theQry:
+    #         print "--> {0}".format(item)
             
-    def testPushSetup(self):
-        print "Testing if the Push Setup works properly"""
-        self.push.testRemoteQuery()
-        self.push.testLocalQuery()
+    def testRemoteDirect(self):
+        """Test to see if making a direct connection to the remote database returns what we expect"""
+        push = Pusher.Pusher()
+
+        #Initalise Connections
+        push.init_remote(self.remoteEngine)
+
+        #Do we get the same items come back from the database
+        #For the Remote
+        remotePush = push.testRemoteQuery()
+        session = self.remoteSession()
+        remoteData = session.query(models.RoomType).all()
+        self.assertEqual(remotePush,remoteData)
+
+
+    def testLocalDirect(self):
+        """Test to see if making a direct connection to the local database returns what we expect"""
+
+        push = Pusher.Pusher()
+
+        push.init_local(self.localEngine)
+        #Do we get the same items come back from the database
+        #For the Remote
+        remotePush = push.testLocalQuery()
+        session =testmeta.Session()
+        remoteData = session.query(models.RoomType).all()
+        self.assertEqual(remotePush,remoteData)        
+
+
+        
+
+
         
 
     def testConnection(self):
