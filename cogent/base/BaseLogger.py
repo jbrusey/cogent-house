b072a9bd8ecfe6f1623e9e5034c03fab04f18f1e
Ross Wilkins <Ross Wilkins@localhost>
2012-11-22 21:15:14 +0000

    see previous

--- cogent/base/BaseLogger.py
+++ cogent/base/BaseLogger.py
@@ -132,6 +132,9 @@ class BaseLogger(object):
                             am.set_seq(int(v))
                             am.set_route(msg.get_route())
                             am.set_hops(msg.get_hops())
+
+                            self.bif.sendMsg(am,dest)
+                            logger.debug("Sending Ack %s to %s:, Hops: %s, Route: %s" % (am.get_seq(), dest, am.get_hops(), am.get_route()))
                             return
                     j += 1
                     return
