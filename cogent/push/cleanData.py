19e0fbccfba01fd7a0b5d3fe27884b8cdc568a7c
Dan Goldsmith <djgoldsmith@googlemail.com>
2013-01-09 13:57:08 +0000

    Updated Cleaning Functions

--- cogent/push/cleanData.py
+++ cogent/push/cleanData.py
@@ -89,8 +89,11 @@ class Cleaner(object):
         for key,value in dupTypes.iteritems():
             if len(value) > 1:
                 log.debug("--> Duplicate Room Type {0} {1}".format(key,value))
+                #firstType = value[0]
+                #log.debug("--> First Room Type {0}".format(firstType))
+                #for roomType in value[1:]:
+                #    log.debug("--> --> Other Types {0}".format(roomType))
 
-        
         log.info("----- Checking roosms-----")         
         theQry = session.query(models.Room)
         dupTypes = {}
@@ -102,36 +105,66 @@ class Cleaner(object):
             if item.location is None:
                 log.debug("--> Room without Location {0}".format(item))
 
-        log.debug("===== CHECKING FOR DUPLICATE ROOM TYPES ====")
-        badLocs = []
+        log.debug("===== CHECKING FOR DUPLICATE ROOMS ====")
+
         for key,value in dupTypes.iteritems():
             if len(value) > 1:
-                log.debug("--> Duplicate Room Type {0} {1}".format(key,value))
+                log.debug("Duplicate Room {0}".format(key))
+                badLocs = []
+                firstRoom = value[0]
+                print "First Room Is {0}".format(firstRoom)
                 for item in value:
                     log.debug("--> Room {0}".format(item))
-                    badCount = 0
-                    for loc in item.location:
-                        log.debug("--> --> Location {0}".format(loc))
-                        #Then check for readings
-                        rdg = loc.filtReadings.first()
-                        log.debug("--> --> --> First Reading {0}".format(rdg))
-                        if rdg is None:
-                            badLocs.append(loc.id)
-                            badCount += 1
-                    if badCount == len(item.location):
-                        log.debug("--> --> Room Has No Valid Locations {0}".format(item))
-                        session.delete(item)
-                        session.commit()
+                    if item.location:
+                        log.debug("--> Has Locations")
+                        badLocs.extend(item.location)
                     else:
-                       log.debug("--> Refactoring so readings have the same Location")
-                       #firstLoc = value[0]
-                       #remLocs = value[1:]
-                       #log.debug("First Location {0}".format(firstLoc))
-                       #log.debug("Remaining Locations {0}".format(remLocs))
-        print badLocs
+                        log.debug("No Locations")
+                        session.delete(item)
+                        session.flush()
+                session.commit()
+                
+                print "Locations"
+                if len(badLocs) == 0:
+                    continue
+                print "Bad Locations"
+                firstLoc = badLocs[0]
+                print "First Location {0}".format(firstLoc)
+                for item in set(badLocs[1:]):
+                    #print "Loc: ",item
+                    #So we Want to fetch the readings
+                    rdg = item.filtReadings.first()
+                    print "First Reading {0}".format(rdg)
+                    theQry = session.query(models.Reading).filter_by(locationId = item.id)
+                    #for item in theQry:
+                    #    item.locationId = firstLoc.id
+                        #print item
+                    # #print theQry
+                    # print firstLoc.id
+                    out = theQry.update({"locationId" : firstLoc.id})
+                    print "{0} Samples moved from {1} to {2}".format(out,rdg,firstLoc.id)
+                    session.flush()
+                    session.commit()
+                    session.delete(item)                    
+                    session.commit()
+
                     
+                for item in value[1:]:
+                    log.debug("--> Room {0}".format(item))
+                    log.debug("--> Locs {0}".format(item.location))
+                    if not item.location:
+                        log.debug("--> Delete")
+                        session.delete(item)
+                        session.flush()
 
-
+                if value != firstLoc:
+                    log.debug("Delete Duplicate Room")
+                    session.delete(item)
+                    session.flush()
+                
+                session.flush()
+                session.commit()
+                
 
     def uploadFakeReadings(self):
         import datetime
@@ -149,8 +182,8 @@ class Cleaner(object):
         session.commit()
 
 if __name__ == "__main__":
-    #cleaner = Cleaner("mysql://root:Ex3lS4ga@127.0.0.1/transferTest")
-    cleaner = Cleaner("mysql://root:Ex3lS4ga@127.0.0.1/mainStore")
+    cleaner = Cleaner("mysql://root:Ex3lS4ga@127.0.0.1/transferTest")
+    #cleaner = Cleaner("mysql://root:Ex3lS4ga@127.0.0.1/mainStore")
     cleaner.cleanLocation()
     cleaner.cleanRooms()
     # cleaner.uploadFakeReadings()
