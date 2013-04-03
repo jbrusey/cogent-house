ae3f8d25660d3f9b92f7ef72bf09a41bc24e64be
Ross Wilkins <Ross Wilkins@localhost>
2013-04-03 10:36:00 +0100

    Added cluster head types to db

--- cogent/alembic/versions/3a4338e38dea_add_cluster_and_energy_typ.py
+++ cogent/alembic/versions/3a4338e38dea_add_cluster_and_energy_typ.py
@@ -30,7 +30,11 @@ def upgrade():
 
 
     op.bulk_insert(nodetype,
-                   [{'id': 10, 'name': "ClusterHead",
+                   [{'id': 10, 'name': "ClusterHead CO2",
+                     'time': "2011-07-10 00:00:00",
+                     'seq': 1,
+                     'updated_seq': 0., 'period': 307200., 'blink': 0., 'configured': '31,4'},
+                     {'id': 11, 'name': "ClusterHead AQ",
                      'time': "2011-07-10 00:00:00",
                      'seq': 1,
                      'updated_seq': 0., 'period': 307200., 'blink': 0., 'configured': '31,4'},
