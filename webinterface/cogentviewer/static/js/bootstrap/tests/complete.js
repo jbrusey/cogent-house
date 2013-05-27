f0210f2a9c4203e9b96d44e59619ca4cd83eed16
djgoldsmith <djgoldsmith@googlemail.com>
2013-05-27 16:30:59 +0100

    Added Static

---
+++ webinterface/cogentviewer/static/js/bootstrap/tests/complete.js
@@ -0,0 +1,17 @@
+require({
+    packages: [
+        { name: 'bootstrap', location: '../../..' }
+    ]
+},[
+    'bootstrap/tests/support',
+    'bootstrap/tests/alert',
+    'bootstrap/tests/button',
+    'bootstrap/tests/dropdown',
+    'bootstrap/tests/modal',
+    'bootstrap/tests/popover',
+    'bootstrap/tests/tab',
+    'bootstrap/tests/tooltip',
+    'bootstrap/tests/scrollspy',
+    'bootstrap/tests/collapse',
+    'bootstrap/tests/carousel'
+], {});
