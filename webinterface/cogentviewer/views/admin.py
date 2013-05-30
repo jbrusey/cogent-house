363d4e3eb043b1ad507ead82e35e1d188e5de368
Ross Wilkins <Ross Wilkins@localhost>
2013-05-30 12:40:57 +0100

    missing view changes

--- webinterface/cogentviewer/views/admin.py
+++ webinterface/cogentviewer/views/admin.py
@@ -2,32 +2,41 @@
 Node Status and Admin
 """
 
-from pyramid.response import Response
-from pyramid.renderers import render_to_response
-import pyramid.url
-
-import sqlalchemy
-
-import logging
-log = logging.getLogger(__name__)
-
-import homepage
-
-import cogentviewer.models.meta as meta
-import cogentviewer.models as models
-
 import time
 import datetime
 from itertools import groupby
 
+from pyramid.response import Response
+from pyramid.renderers import render_to_response
+from pyramid.view import view_config
+import pyramid.url
+
+import sqlalchemy
+
+#import logging
+#log = logging.getLogger(__name__)
+
+import cogentviewer.models.meta as meta
+import cogentviewer.models as models
+import cogentviewer.views.homepage
+
+
 def _groupDate(item):
-    """ Extract the date from a Readings datetime object"""
+    """ Extract the date from a Readings datetime object
+    @var item:  Reading object to take date from
+
+    @return:  A datetime object for this reading
+    """
     return item.time.date()
 
+@view_config(route_name='status', renderer='status.mak', permission="view")
 def status(request):
+    """Show Status page"""
     outDict = {}
     outDict["headLinks"] = homepage.genHeadUrls(request)
     outDict["sideLinks"] = homepage.genSideUrls(request)
+    theUser = homepage.getUser(request)
+    outDict["user"] = theUser.username
 
     outDict["pgTitle"] = "Node Status"
     #outDict["deployments"] =deps
@@ -41,14 +50,16 @@ def status(request):
                               request=request)
 
 
-def _getStatus(request):
-    pass
-
-
+@view_config(route_name='admin', renderer='admin.mak', permission="view")
 def admin(request):
+    """
+    Show Admin Page
+    """
     outDict = {}
     outDict["headLinks"] = homepage.genHeadUrls(request)
     outDict["sideLinks"] = homepage.genSideUrls(request)
+    theUser = homepage.getUser(request)
+    outDict["user"] = theUser.username
 
     outDict["pgTitle"] = "Admin Interface"
 
