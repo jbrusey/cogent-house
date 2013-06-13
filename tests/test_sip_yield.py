4ced01fd8e11f281dda5f9f5ea38eee29ee85d04
James Brusey <james.brusey@gmail.com>
2013-06-13 23:01:27 +0100

    oops - fix import of math

--- tests/test_sip_yield.py
+++ tests/test_sip_yield.py
@@ -8,8 +8,12 @@ class TestSipYield(unittest.TestCase):
         self.assertEquals(100.,calc_yield(7, 9, 15))
 
         self.assertEquals(100., calc_yield(286, 71, 100))
-        self.assertEquals(100., calc_yield(286, 241, 24))
+        self.assertEquals(100., calc_yield(2, 255, 0))
+        self.assertEquals(100., calc_yield(258, 255, 0))
+        self.assertEquals(100., calc_yield(286, 241, 270))
 
         self.assertEquals(10., calc_yield(1, 1, 10))
 
-        self.assertEquals(100., calc_yield(1, 255, 0))
+
+if __name__ == "__main__":
+    unittest.main()
