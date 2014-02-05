d8e8146cead37801e7c6deb82848166243421af2
Dan Goldsmith <djgoldsmith@googlemail.com>
2014-02-05 10:11:14 +0000

    Updated reports to accept mailto option

--- cogent/report/__init__.py
+++ cogent/report/__init__.py
@@ -4,7 +4,6 @@ from cogent.report.lowbat import lowBat
 from cogent.report.packetyield import packetYield
 from cogent.report.ccyield import ccYield
 
-
 reports = [ 
             ('Nodes with low battery in last 24 hours', lowBat),
             ('Packet yield', packetYield),
