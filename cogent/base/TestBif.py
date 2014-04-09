db2e01084e3e4f11fc420551cf62e33d2049ac68
James Brusey <james.brusey@gmail.com>
2014-04-09 11:10:07 +0100

    pylint fixes

--- cogent/base/TestBif.py
+++ cogent/base/TestBif.py
@@ -1,87 +1,97 @@
-#
-# Test code for feeding BaseLogger with some packets.
-#
-# J. Brusey, May 2011
+""" 
+Test code for feeding BaseLogger with some packets.
 
+J. Brusey, May 2011
+"""
 import sys
 import os
 sys.path.append(os.environ["TOSROOT"] + "/support/sdk/python")
 sys.path.append("../..")
-from cogent.node import *
-from tinyos.message import MoteIF 
-import time
-from cogent.base.model import *
-from Queue import Queue, Empty
-from BaseLogger import BaseLogger
+from cogent.node import StateMsg
+# from cogent.base.model import
+from Queue import Queue
+from cogent.base.BaseLogger import BaseLogger
+import logging
 
-
-# Note: MoteIF doesn't currently support coming directly off the serial interface
+# Note: MoteIF doesn't currently support coming directly off the
+# serial interface
 #
 class TestBif(object):
+    """ TestBif is a testing version of BaseIF to allow simulation of
+    messages from the SerialForwarder
+    """
     def __init__(self):
         self.queue = Queue()
-		
+
     def receive(self, msg):
+        """ receive a single state message - in this case, will be
+        called by test code to add it to the queue.
+        """
         self.queue.put(msg)
 
     def sendMsg(self, msg):
-        # assumption is that msg is a ConfigMsg
-        print "sendMsg",msg
+        """ support sending (from BaseLogger) of any messages """
+        print "sendMsg", msg
 
     def finishAll(self):
+        """ finish - currently just a dummy """
         pass
-    
-
-if __name__ == '__main__':
-    import logging
-    tb = TestBif()
 
 
-    sm = StateMsg(addr=22)
-    sm.set_ctp_parent_id(101)
-    sm.set_timestamp(307200)
-    sm.set_special(0xc7)
-    sm.setElement_packed_state_mask(0, 1)
-    sm.setElement_packed_state_mask(1, 0)
-    sm.setElement_packed_state_mask(2, 0)
-    sm.setElement_packed_state(0, 25.5)
-    
+def main():
+    """ run all tests """
+    testbif = TestBif()
 
-    tb.receive(sm)
 
-    sm = StateV1Msg(addr=23)
-    sm.set_ctp_parent_id(101)
-    sm.set_timestamp(307200)
-    sm.set_special(0xc7)
-    sm.setElement_packed_state_mask(0, 1)
-    sm.setElement_packed_state_mask(1, 0)
-    sm.setElement_packed_state(0, 22.5)
-    
+    s_msg = StateMsg(addr=22)
+    s_msg.set_ctp_parent_id(101)
+    s_msg.set_timestamp(307200)
+    s_msg.set_special(0xc7)
+    s_msg.setElement_packed_state_mask(0, 1)
+    s_msg.setElement_packed_state_mask(1, 0)
+    s_msg.setElement_packed_state_mask(2, 0)
+    s_msg.setElement_packed_state(0, 25.5)
 
-    tb.receive(sm)
+
+    testbif.receive(s_msg)
+
+    # s_msg = StateV1Msg(addr=23)
+    # s_msg.set_ctp_parent_id(101)
+    # s_msg.set_timestamp(307200)
+    # s_msg.set_special(0xc7)
+    # s_msg.setElement_packed_state_mask(0, 1)
+    # s_msg.setElement_packed_state_mask(1, 0)
+    # s_msg.setElement_packed_state(0, 22.5)
+
+
+    # testbif.receive(s_msg)
 
     # test what happens when you have an unknown nodetype
-    sm = StateMsg(addr=(4096*5+1))
-    sm.set_ctp_parent_id(101)
-    sm.set_timestamp(307200)
-    sm.set_special(0xc7)
-    sm.setElement_packed_state_mask(0, 1)
-    sm.setElement_packed_state_mask(1, 0)
-    sm.setElement_packed_state_mask(2, 0)
-    sm.setElement_packed_state(0, 25.5)
-    
+    s_msg = StateMsg(addr=(4096*5+1))
+    s_msg.set_ctp_parent_id(101)
+    s_msg.set_timestamp(307200)
+    s_msg.set_special(0xc7)
+    s_msg.setElement_packed_state_mask(0, 1)
+    s_msg.setElement_packed_state_mask(1, 0)
+    s_msg.setElement_packed_state_mask(2, 0)
+    s_msg.setElement_packed_state(0, 25.5)
 
-    tb.receive(sm)
+
+    testbif.receive(s_msg)
 
 
 
     logging.basicConfig(#filename="/tmp/BaseLogger.log",
                         #filemode="a",
                         format="%(asctime)s %(levelname)s %(message)s",
-                        level=logging.DEBUG)    
-    lm = BaseLogger(bif=tb, dbfile='sqlite:///testbif.db')# 'mysql://localhost/ch')
-    lm.create_tables()
-    lm.run()
+                        level=logging.DEBUG)
+    base_logger = BaseLogger(bif=testbif,
+                             dbfile='sqlite:///testbif.db')
+    # 'mysql://localhost/ch')
+    base_logger.create_tables()
+    base_logger.run()
 
 
-        
+
+if __name__ == '__main__':
+    main()
