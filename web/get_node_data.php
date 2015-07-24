09ac4af5f7b2c436a144b01f479491eac201ab0a
Ross Wilkins <ross@rosss-mbp-2.coventry.ac.uk>
2015-07-24 15:19:03 +0100

    Fix file paths

--- web/get_node_data.php
+++ web/get_node_data.php
@@ -1,6 +1,6 @@
 <?php
 // read node files and put last json in dicts..
-foreach (glob("tmp/node_*.log") as $filename) {
+foreach (glob("/tmp/node_*.log") as $filename) {
   $lines = file($filename);
   foreach ($lines as $line_num => $line) {
     echo $line."\n";
