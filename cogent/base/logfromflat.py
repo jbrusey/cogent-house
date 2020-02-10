368fa57881e62a1a1b9533589b749d2e5e4b698f
James Brusey <james.brusey@gmail.com>
2020-02-10 17:37:19 +0000

    python3 and logfromflat

---
+++ cogent/base/logfromflat.py
@@ -0,0 +1,274 @@
+#
+# logfromflat
+#
+# Convert flat file containing JSON data into mysql reading records.
+#
+# Author: J. Brusey, November 2019
+#
+# Modification history:
+#
+
+"""LogFromFlat - convert json to mysql
+
+"""
+
+import logging
+import math
+import json
+from optparse import OptionParser
+
+from datetime import timedelta, datetime
+
+#import time
+
+from cogent.base.model import (Reading,
+                               NodeState,
+                               SensorType,
+                               Node)
+
+import cogent.base.model as models
+import cogent.base.model.meta as meta
+
+LOGGER = logging.getLogger("ch.base")
+
+#DBFILE = "mysql://chuser@localhost/ch"
+DBFILE = "sqlite:///test.db"
+
+PROCESSED_FILES = "processed_files.txt"
+
+from sqlalchemy import create_engine, and_
+import sqlalchemy
+
+def duplicate_packet(session, receipt_time, node_id, localtime):
+    """ duplicate packets can occur because in a large network,
+    the duplicate packet cache used is not sufficient. If such
+    packets occur, then they will have the same node id, same
+    local time and arrive within a few seconds of each other. In
+    some cases, the first received copy may be corrupt and this is
+    not dealt with within this code yet.
+    """
+    assert isinstance(receipt_time, datetime)
+    earliest = receipt_time - timedelta(minutes=1)
+    return session.query(NodeState).filter(
+        and_(NodeState.nodeId==node_id,
+             NodeState.localtime==localtime,
+             NodeState.time > earliest)).first() is not None
+
+def add_node(session, node_id):
+    """ add a database entry for a node
+    """
+    try:
+        session.add(Node(id=node_id,
+                         locationId=None,
+            nodeTypeId=(node_id / 4096)))
+        session.commit()
+    except Exception:
+        session.rollback()
+        LOGGER.exception("can't add node %d" % node_id)
+
+
+class LogFromFlat(object):
+    """ LogFromFlat class reads a JSON file and writes sensor readings to
+    the database.
+    """
+    def __init__(self, dbfile=DBFILE):
+        """ create a new LogFromFlat that reads from jsonfile and writes to dbfile
+        """
+        self.engine = create_engine(dbfile, echo=False)
+        models.initialise_sql(self.engine)
+
+        self.log = logging.getLogger("logfromflat")
+        self.create_tables()
+
+        
+    def create_tables(self):
+        """ create any missing tables using sqlalchemy
+        """
+        session = meta.Session()
+        try:
+            models.populateData.init_data(session)
+            if session.query(SensorType).get(0) is None:
+                raise Exception("SensorType must be populated by alembic " +
+                                "before starting LogFromFlat")
+        except:
+            session.rollback()
+            raise
+        finally:
+            session.close()
+                             
+
+    def store_state(self, msg):
+        """ receive and process a message object from the base station
+        """
+        current_time = datetime.utcfromtimestamp(msg['server_time'])
+        try:
+            session = meta.Session()
+
+            node_id = msg['sender']
+            parent_id = msg['parent']
+            seq = msg['seq']
+            rssi_val = msg['rssi']
+
+            node = session.query(Node).get(node_id)
+            loc_id = None
+            if node is None:
+                add_node(session, node_id)
+            else:
+                loc_id = node.locationId
+
+
+            if duplicate_packet(session=session,
+                                receipt_time=datetime.utcfromtimestamp(msg['server_time']),
+                                node_id=node_id,
+                                localtime=msg['localtime']):
+                LOGGER.info("duplicate packet %d->%d, %d %s" %
+                            (node_id, parent_id, msg['localtime'], str(msg)))
+
+                return False
+
+            # write a node state row
+            node_state = NodeState(time=datetime.utcfromtimestamp(msg['server_time']),
+                                   nodeId=node_id,
+                                   parent=parent_id,
+                                   localtime=msg['localtime'],
+                                   seq_num=seq,
+                                   rssi = rssi_val)
+            session.add(node_state)
+
+            for type_id, value in list(msg.items()):
+                # skip any non-numeric type_ids
+                try:
+                    type_id = int(type_id)
+                except ValueError:
+                    continue
+                
+                if math.isinf(value) or math.isnan(value):
+                    value = None
+
+                r = Reading(time=current_time,
+                            nodeId=node_id,
+                            typeId=type_id,
+                            locationId=loc_id,
+                            value=value)
+                try:
+                    session.add(r)
+                    session.flush()
+
+                except sqlalchemy.exc.IntegrityError as e:
+                    self.log.error("Unable to store reading, checking if node type exists")
+                    self.log.error(e)
+                    session.rollback()
+                    s = session.query(SensorType).filter_by(id=type_id).first()
+                    if s is None:
+                        s = SensorType(id=type_id, name="UNKNOWN")
+                        session.add(s)
+                        self.log.info("Adding new sensortype")
+                        session.flush()
+                        session.add(r)
+                        session.flush()                            
+                    else:
+                        self.log.error("Sensor type exists")
+
+
+            self.log.debug("reading: {}".format(node_state))
+            session.commit()
+
+
+        except Exception as exc:
+            session.rollback()
+            self.log.exception("error during storing (reading): " + str(exc))
+        finally:
+            session.close()
+
+        return True
+
+
+
+    def process_file(self, jsonfile):
+        """ process a file from JSON into the database """
+        with open(jsonfile, "r") as ff:
+            for ll in ff:
+                print(ll)
+                msg = json.loads(ll)
+                self.store_state(msg)
+
+    def process_dir(self, datadir):
+        """process directory containing json log files into the database and
+        update the log of files processed"""
+
+        processed_set = set()
+
+        pf = datadir / PROCESSED_FILES
+        if pf.exists():
+        
+            with open(pf, 'r') as processed_files:
+
+                for row in processed_files:
+                    processed_set.add(row.rstrip())
+
+        for logfile in datadir.glob('*.log'):
+            if logfile.name not in processed_set and not logfile.is_dir():
+                self.process_file(logfile)
+
+            processed_set.add(logfile.name)
+
+        with open(pf, 'w') as processed_files:
+            for entry in processed_set:
+                processed_files.write(entry + '\n')
+            
+
+            
+                
+if __name__ == '__main__': # pragma: no cover
+    parser = OptionParser()
+    parser.add_option("-i", "--input",
+                      help="json file containing sensor readings",
+                      default=None)
+    parser.add_option("-l", "--log-level",
+                      help="Set log level to LEVEL: debug,info,warning,error",
+                      default="debug",
+                      metavar="LEVEL")
+
+    parser.add_option("-f", "--log-file",
+                      help="Log file to use (Default /var/log/ch/Baselogger.log",
+                      default="/var/log/ch/LogFromFlat.log")
+
+    parser.add_option("-t", "--log-terminal",
+                      help="Echo Logging output to terminal",
+                      action="store_true",
+                      default=False)
+
+
+    (options, args) = parser.parse_args()
+    if len(args) != 0:
+        parser.error("incorrect number of arguments")
+
+    lvlmap = {"debug": logging.DEBUG,
+              "info": logging.INFO,
+              "warning": logging.WARNING,
+              "error": logging.ERROR,
+              "critical": logging.CRITICAL}
+
+    if options.log_level not in lvlmap:
+        parser.error("invalid LEVEL: " + options.log_level)
+
+    logfile = options.log_file
+
+    logging.basicConfig(filename=logfile,
+                        filemode="a",
+                        format="%(asctime)s %(levelname)s %(message)s",
+                        level=lvlmap[options.log_level])
+
+    #And if we want to echo the output on the terminal
+    logterm = options.log_terminal
+    if logterm:
+        console = logging.StreamHandler()
+        console.setLevel(lvlmap[options.log_level])
+        formatter = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+        console.setFormatter(formatter)
+        logging.getLogger('').addHandler(console)
+
+
+
+    logging.info("Starting LogFromFlat with log-level %s" % (options.log_level))
+    lm = LogFromFlat(jsonfile=options.input)
---
+++ tests/test_logfromflat.py
@@ -0,0 +1,188 @@
+""" test LogFromFlat
+
+author: J. Brusey, November 2019
+
+"""
+
+
+from cogent.base.logfromflat import LogFromFlat
+from unittest.mock import patch, mock_open
+from sqlalchemy import create_engine
+from cogent.base.model import (init_model,
+                               meta,
+                               Bitset,
+                               Location,
+                               Node,
+                               NodeState,
+                               NodeType,
+                               Reading,
+                               SensorType,
+                               Room,
+                               RoomType,
+                               House,
+                               Deployment)
+                               
+from datetime import datetime
+from pathlib import Path
+
+DBURL = "sqlite:///:memory:"
+
+
+@patch('cogent.base.logfromflat.LogFromFlat.create_tables')
+@patch('cogent.base.logfromflat.create_engine')
+@patch('cogent.base.logfromflat.models')
+def test_process_file(models, create_engine, create_tables):
+    """mock store_state and check that process file reads from a json
+    file and passes each line to store_state """
+
+    with patch('cogent.base.logfromflat.LogFromFlat.store_state') as ss:
+        with patch('cogent.base.logfromflat.open', mock_open(read_data="""{"0": 18, "1": -5, "sender": 235}
+        {"0": 7, "1": 0, "sender": 236}"""
+        )):
+            lff = LogFromFlat(dbfile="y")
+            lff.process_file("x")
+
+        ss.assert_any_call({'0': 18, '1': -5, 'sender': 235})
+        ss.assert_any_call({'0': 7, '1': 0, 'sender': 236})
+
+def dummy_deployment():
+
+    session = meta.Session()
+    try:
+
+        dep = Deployment(name='TestDep',
+                               description='',
+                               startDate=datetime.utcnow(),
+                               endDate=None)
+        session.add(dep)
+        session.commit()
+
+        #Add a room type
+
+        rt = RoomType(name="Bedroom")
+        session.add(rt)
+        session.commit()
+
+        #Add a house
+        h = House(deploymentId=1,
+                  address="unknown",
+                  startDate=datetime.utcnow())
+
+        session.add(h)
+        session.commit()
+
+
+        r = Room(roomTypeId=rt.id,
+                       name="BedroomA")
+
+        session.add(r)
+        session.commit()
+
+        ll = Location(houseId=h.id,
+                      roomId=r.id)
+        session.add(ll)
+        session.commit()
+        assert ll.id is not None
+
+        nt = NodeType(time=datetime.utcnow(),
+                      id=0,
+                      name="base",
+                      seq=1,
+                      updated_seq=0,
+                      period=15*1024,
+                      configured=Bitset(size=14))
+
+        session.add(nt)
+        session.commit()
+        assert nt.id is not None
+
+        n = Node(id=28710,
+                 locationId=ll.id,
+                 nodeTypeId=nt.id)
+        session.add(n)
+
+        st = SensorType(id=0,
+                        name="Temperature",
+                        code="Tmp",
+                        units="deg.C")
+
+        session.add(st)
+        session.commit()
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+        
+def test_store_state():
+    """ test that store_state correctly stores incoming data """
+
+
+    engine = create_engine(DBURL, echo=False)
+    engine.execute("pragma foreign_keys=on")
+    init_model(engine)
+    metadata = meta.Base.metadata
+    metadata.create_all(engine)
+
+
+    # set up database with some basic data.
+    dummy_deployment()
+
+
+    lff = LogFromFlat(dbfile=DBURL)
+
+
+    res = lff.store_state({"0": 10.1361722946167, "1": -1.933643397933338e-05, "2": 95.4524154663086, "3": 0.00018446370086167008, "server_time": 1581266093.331143, "sender": 28710, "6": 2.999267578125, "7": 0.0, "43": 3698.0, "13": 510.0, "parent": 40969, "rssi": -91, "seq": 22, "localtime": 643594668})
+
+    assert res
+
+    # duplicate should return false
+    res = lff.store_state({"0": 10.1361722946167, "1": -1.933643397933338e-05, "2": 95.4524154663086, "3": 0.00018446370086167008, "server_time": 1581266093.331143, "sender": 28710, "6": 2.999267578125, "7": 0.0, "43": 3698.0, "13": 510.0, "parent": 40969, "rssi": -91, "seq": 22, "localtime": 643594668})
+
+    assert not res
+
+    session = meta.Session()
+    try:
+        reading = session.query(Reading).filter(Node.id == 28710, Node.nodeTypeId == 0).first()
+
+        assert reading.locationId is not None
+
+        assert reading.value == 10.1361722946167
+
+        nodestate = session.query(NodeState).filter(Node.id == 28710).one()
+
+        assert nodestate.localtime == 643594668
+    except:
+        session.rollback()
+        raise
+    finally:
+        session.close()
+
+        
+@patch('cogent.base.logfromflat.LogFromFlat.process_file')
+def test_process_dir(process_file):
+    import tempfile
+    with tempfile.TemporaryDirectory() as tempdir:
+        temppath = Path(tempdir)
+        with open(temppath / 'a.log', 'w') as f:
+            f.write('{"0": 1}')
+        with open(temppath / 'b.log', 'w') as f:
+            f.write('{"0": 1}')
+        lff = LogFromFlat(dbfile=DBURL)
+        lff.process_dir(temppath)
+        process_file.assert_any_call(temppath / 'a.log')
+        process_file.assert_any_call(temppath / 'b.log')
+
+        with open(temppath / 'processed_files.txt') as processed_files:
+            fileset = []
+            for row in processed_files:
+                fileset.append(row.rstrip())
+
+            assert len(fileset) == 2
+            assert 'a.log' in fileset
+            assert 'b.log' in fileset
+                
+        
+        
+        
+        
