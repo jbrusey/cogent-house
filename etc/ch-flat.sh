1419782ae238f8e2c53fba15d2d578d2104db243
James Brusey <james.brusey@gmail.com>
2022-11-06 10:33:26 +0000

    add sleep of 10 seconds to ensure ch-sf is up

--- etc/ch-flat.sh
+++ etc/ch-flat.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 #
 # start flatlogger
+# Modification history:
+# 1. 4/2/2020 jpb add sleep of 10 seconds to ensure ch-sf is up
+sleep 10
 . /opt/tinyos-main/tinyos.sh
-exec python -m pulp.base.FlatLogger -t /tmp/ -o /opt/pulp/data -d 600.0 -f /var/log/ch/FlatLogger.log
+exec python -m pulp.base.FlatLogger \
+	--log-level debug\
+	-t /tmp/ -o /opt/pulp/data -d 600.0 -f /var/log/ch/FlatLogger.log
